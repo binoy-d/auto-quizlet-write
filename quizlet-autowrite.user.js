ea18955cfe86c39982a86cf90071fdeffe81c279
Daniel Binoy <bleeblob43@gmail.com>
2018-03-13 14:41:24 -0700

    Update quizlet-autowrite.user.js

--- quizlet-autowrite.user.js
+++ quizlet-autowrite.user.js
@@ -12,6 +12,20 @@
     'use strict';
     window.onload = function() {
 
-        setInterval(function () {document.getElementById('user-answer').value = "booty";document.getElementById("js-learnModeAnswerButton").click();}, 500);
+        setInterval(function () {
+            document.getElementById('user-answer').value = "booty";
+            document.getElementById("js-learnModeAnswerButton").click();
+var aTags = document.getElementsByClassName("UILink");
+var searchText = "Override: I was right";
+var found;
+
+for (var i = 0; i < aTags.length; i++) {
+  if (aTags[i].textContent == searchText) {
+    found = aTags[i];
+    break;
+  }
+}
+            found.click();
+        }, 500);
     };
 })();
