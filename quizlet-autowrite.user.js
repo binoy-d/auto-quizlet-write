a6ad6d546b9a2b3c3c65d1edf1fd35a983147af1
Daniel Binoy <bleeblob43@gmail.com>
2018-03-13 14:30:06 -0700

    Update quizlet-autowrite.user.js

--- quizlet-autowrite.user.js
+++ quizlet-autowrite.user.js
@@ -1,3 +1,13 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Automatically put in 'booty' as the answer and click the answer button
+// @author       You
+// @match        https://quizlet.com/*/write
+// @grant        none
+// ==/UserScript==
+
 (function() {
     'use strict';
     window.onload = function() {
