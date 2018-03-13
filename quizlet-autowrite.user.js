8947d06c83e7dc198f7242876683016ab253ff36
Daniel Binoy <bleeblob43@gmail.com>
2018-03-13 14:26:26 -0700

    Create quizlet-autowrite.user.js

---
+++ quizlet-autowrite.user.js
@@ -0,0 +1,7 @@
+(function() {
+    'use strict';
+    window.onload = function() {
+
+        setInterval(function () {document.getElementById('user-answer').value = "booty";document.getElementById("js-learnModeAnswerButton").click();}, 500);
+    };
+})();
