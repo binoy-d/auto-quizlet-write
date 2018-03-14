1a325a7beda37f85520cca8d4451ce0b895038f6
Daniel Binoy <bleeblob43@gmail.com>
2018-03-13 17:09:20 -0700

    Update quizlet-autowrite.user.js

--- quizlet-autowrite.user.js
+++ quizlet-autowrite.user.js
@@ -15,15 +15,15 @@
         setInterval(function () {
             document.getElementById('user-answer').value = "booty";
             document.getElementById("js-learnModeAnswerButton").click();
-var aTags = document.getElementsByClassName("UILink");
-var searchText = "Override: I was right";
-var found;
+            var aTags = document.getElementsByClassName("UILink");
+            var searchText = "Override: I was right";
+            var found;
 
-for (var i = 0; i < aTags.length; i++) {
-  if (aTags[i].textContent == searchText) {
-    found = aTags[i];
-    break;
-  }
+            for (var i = 0; i < aTags.length; i++) {
+                if (aTags[i].textContent == searchText) {
+                    found = aTags[i];
+                      break;
+            }
 }
             found.click();
         }, 100);
